86f113843e9fd3ab937ef9b0b68adfbb2f9b1c64
meerasndr <mirage.sundial@gmail.com>
2021-06-15 21:31:00 +0530

    read code from text file

--- 2015/go/1/main.go
+++ 2015/go/1/main.go
@@ -1,10 +1,16 @@
 package main
+
 import (
 	"fmt"
+	"io/ioutil"
 )
 
 func main(){
 	floor := 0
+	code, err := ioutil.ReadFile("code.txt")
+	if err != nil {
+		panic(err)
+	}
 	for i := 0; i < len(code); i++ {
 		if code[i] == '('{
 			floor++
