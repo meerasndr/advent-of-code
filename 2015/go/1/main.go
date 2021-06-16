1d3cc5010313ec8b62c33c501449f03682694778
meerasndr <mirage.sundial@gmail.com>
2021-06-16 08:38:42 +0530

    refactor problem 1

--- 2015/go/1/main.go
+++ 2015/go/1/main.go
@@ -11,31 +11,20 @@ func main(){
 		panic(err)
 	}
 	getFinalFloor(code)
-	firstBasementHit(code)
 }
 func getFinalFloor(code []byte){
 	floor := 0
-	for i := 0; i < len(code); i++ {
-		if code[i] == '('{
-			floor++
-		} else{
-			floor--
-		}
-	}
-	fmt.Println(floor)
-}
-
-func firstBasementHit(code []byte){
-	floor := 0
+	basement := 0
 	for i := 0; i < len(code); i++ {
 		if code[i] == '('{
 			floor++
 		} else {
 			floor--
 		}
-		if floor == -1 {
-			fmt.Println(i + 1)
-			break
+		if floor == -1 && basement == 0{
+			basement = 1
+			fmt.Println("First basement hit:", i + 1)
 		}
 	}
+	fmt.Println("Final floor:", floor)
 }
