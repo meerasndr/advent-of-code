5ee266a33f25875bf4ded955f15e8155c3653db9
meerasndr <mirage.sundial@gmail.com>
2021-06-16 09:41:34 +0530

    problem 2 first part

---
+++ 2015/go/2/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"sort"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	//Open list.txt for reading
+	file, _ := os.Open("list.txt")
+	//Line-by-line scanner
+	scanner := bufio.NewScanner(file)
+	total := 0
+	//For each line in the scanner
+	for scanner.Scan(){
+		//Splitting at x and removing x from the input
+		//The split string is stored as an array
+		arr := strings.Split(scanner.Text(), "x")
+		n := []int{}
+		//Convert string input to integer
+		for _, strval := range arr {
+			intval, _ := strconv.Atoi(strval)
+			n = append(n, intval)
+		}
+		//sort the integer array
+		sort.Ints(n)
+		// surface area calc
+		total += 2 * (n[0] * n[1] + n[1] * n[2] + n[2] * n[0]) + n[0] * n[1]
+	}
+	fmt.Println(total)
+}
